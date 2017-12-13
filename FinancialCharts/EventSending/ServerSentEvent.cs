f83756a448a206ef649cfe1e49fd0589462a5aa5
Alexander <alexiceanroid@gmail.com>
2017-12-13 09:10:35 +0300

    initial

---
+++ FinancialCharts/EventSending/ServerSentEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialCharts.EventSending
+{
+    public class ServerSentEvent
+    {
+        public string Id { get; set; }
+
+        public string Type { get; set; }
+
+        public IList<string> Data { get; set; }
+
+    }
+}
---
+++ FinancialCharts/Middleware/ServerSentEventsMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace FinancialCharts.Middleware
+{
+    public class ServerSentEventsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ServerSentEventsService _serverSentEventsService;
+
+        public ServerSentEventsMiddleware(RequestDelegate next, ServerSentEventsService service)
+        {
+            _next = next;
+            _serverSentEventsService = service;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers["Accept"] == "text/event-stream")
+            {
+                context.Response.ContentType = "text/event-stream";
+                context.Response.Body.Flush();
+
+                ServerSentEventsClient client = new ServerSentEventsClient(context.Response);
+                Guid clientId = _serverSentEventsService.AddClient(client);
+
+                context.RequestAborted.WaitHandle.WaitOne();
+
+                _serverSentEventsService.RemoveClient(clientId);
+
+                return Task.FromResult(true);
+            }
+            else
+            {
+                return _next(context);
+            }
+
+        }
+    }
+}
